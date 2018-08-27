31700c5192a2277392ca4ae51132ec660477e2f4
Cesar Acual <chechaacual@gmail.com>
2018-08-26 23:41:27 -0600

    Creación de migraciones

--- database/migrations/2018_08_27_052528_create_tickets_table.php
+++ database/migrations/2018_08_27_052528_create_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateTicketsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,16 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('tickets', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+
+			$table->string('title', 200);
+			$table->enum('status', ['open', 'closed']);
+
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users');
+
 			$table->nullableTimestamps();
 		});
 	}
@@ -30,7 +33,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('tickets');
 	}
 
 }
