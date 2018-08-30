a9eea17b910b684c60c82d161e2f892f753f6264
Cesar Acual <chechaacual@gmail.com>
2018-08-29 23:23:10 -0600

    Uso de herencia para reusar métodos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder {
         ));
 
         $this->call('UserTableSeeder');
+        $this->call('TicketTableSeeder');
 	}
 
     public function truncateTables(array $tables)
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,37 +1,38 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use Faker\Generator;
 use TeachMe\Entities\User;
 use Faker\Factory as Faker;
 
-class UserTableSeeder extends Seeder {
+class UserTableSeeder extends BaseSeeder {
 
     public function run()
     {
         $this->createAdmin();
-        
-        $this->createUsers(50);
+
+        $this->createMultiple(50);
     }
 
     private function createAdmin()
     {
-        User::create([
+        $this->create([
             'name' => 'Cesar Acual',
             'email' => 'chechaacual@gmail.com',
             'password' => bcrypt('admin')
         ]);
     }
 
-    public function createUsers($total)
+    public function getModel()
     {
-        $faker = Faker::create();
+        return new User();
+    }
 
-        for ($i = 1; $i <= $total; $i++) {
-            User::create([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => bcrypt('secret')
-            ]);
-        }
+    public function getDummyData(Generator $faker, array $customValues = array())
+    {
+        return [
+            'name' => $faker->name,
+            'email' => $faker->email,
+            'password' => bcrypt('secret')
+        ];
     }
 }
