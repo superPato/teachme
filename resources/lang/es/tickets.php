7e8a7480d775b99e7a3cf9e9108062091fa859ce
Cesar Acual <chechaacual@gmail.com>
2018-09-19 18:34:11 -0600

    Uso del componente de traducciones

---
+++ resources/lang/es/tickets.php
@@ -0,0 +1,31 @@
+<?php 
+
+return [
+
+	'latest_title'  => 'Solicitudes recientes',
+	'open_title'    => 'Solicitudes abiertas',
+	'closed_title'  => 'Tutoriales',
+	'pupular_title' => 'Solicitudes populares',
+
+	'latest_total' => '{0} No hay solicitudes recientes'
+					. '|{1} Sólo hay una solicitud reciente'
+					. '|[2,Inf] Hay :count solicitudes recientes',
+
+	'open_total' => '{0} No hay solicitudes abiertas'
+					. '|{1} Sólo hay una solicitud abierta'
+					. '|[2,Inf] Hay :count solicitudes abiertas',
+
+	'closed_total' => '{0} No hay solicitudes tutoriales'
+					. '|{1} Sólo hay una solicitud tutorial'
+					. '|[2,Inf] Hay :count solicitudes tutoriales',
+
+	'popular_total' => '{0} No hay solicitudes populares'
+					. '|{1} Sólo hay una solicitud pupular'
+					. '|[2,Inf] Hay :count solicitudes populares',
+
+	'status' => [
+		'open'   => 'Abierta',
+		'closed' => 'Finalizadas'
+	]
+
+];
