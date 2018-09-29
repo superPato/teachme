c0b277b089b7fdfbff58a55a4afcde04ca4e16c0
Cesar Acual <chechaacual@gmail.com>
2018-09-28 23:12:53 -0600

    Mensajes de Alert con Javascript y Bootstrap

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var alert = new Alert('#notifications');
+
     $('.btn-vote').click(function (e) {
         e.preventDefault();
 
@@ -11,17 +13,27 @@ $(document).ready(function () {
 
         var action = form.attr('action').replace(':id', id);
 
-        console.log(form.serialize());
-
         button.addClass('hidden');
 
         $.post(action, form.serialize(), function (response) {
             // alert
             // update count votes
             ticket.find('.btn-unvote').removeClass('hidden');
+
+            alert.success("¡Gracias por tu voto!");
+
+
+            var voteCount = ticket.find('.votes-count');
+            var votos = parseInt(voteCount.text().split(' ')[0]);  // 5 votos
+            votos++;
+
+            voteCount.text(votos === 1 ? '1 voto' : votos + ' votos');
+
         }).fail(function () {
             // print error message
             button.removeClass('hidden');
+
+            alert.error("Ocurrió un error :(");
         });
     });
 
